db507b1a581e84460b17a67257651381f76e13a6
Sarbjit <srbcheema1@gmail.com>
2020-05-14 23:02:37 +0530

    working fine

--- main.py
+++ main.py
@@ -5,27 +5,11 @@ from dynamic_plot import DynamicPlot
 
 
 def main():
-    '''set the simulated annealing algorithm params'''
-    temp = 10
-    stopping_temp = 0.00000001
-    alpha = 0.9998
-    stopping_iter = 1000000000
-
-    '''set the dimensions of the grid'''
-    size_width = 100
-    size_height = 100
-
-    '''set the number of nodes'''
-    population_size = 120
-
-    '''generate random list of nodes'''
     cities = City.load_cities('./data/data1.txt')
     graph = Graph(cities)
+    init_sol = graph.nearestNeighbourSolution()
 
-    '''run simulated annealing algorithm with 2-opt'''
-    sa = SimulatedAnnealing(graph, temp, alpha, stopping_temp, stopping_iter)
-    history = sa.anneal()
-    print(len(history))
+    history = SimulatedAnnealing(graph, init_sol, 0.9998, 10, 0.0000001, 1000000).anneal()
     DynamicPlot().show(cities,history)
 
 if __name__ == "__main__":
