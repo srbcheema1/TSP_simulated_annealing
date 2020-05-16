4c65efe0547ccdaf1ba7f206267307dd1ceec74a
Sarbjit <srbcheema1@gmail.com>
2020-05-17 00:32:20 +0530

    full visualization

--- dynamic_plot.py
+++ dynamic_plot.py
@@ -6,59 +6,65 @@ class DynamicPlot():
 	def __init__(self):
 		plt.ion() # plot interactive mode ON
 		self.figure, self.ax = plt.subplots(2,2)
-		self.cyan_line, = self.ax[0,0].plot([],[], 'co-') # intermediate path
-		self.green_line, = self.ax[0,0].plot([],[], 'go-') # final path
-		self.red_line, = self.ax[0,0].plot([],[], 'ro') # depot
-		self.ax[0,0].set_title("Simulated Annealing")
+		self.cyan_line, = self.ax[1,0].plot([],[], 'co-') # intermediate path
+		self.green_line, = self.ax[1,0].plot([],[], 'go-') # final path
+		self.red_line, = self.ax[1,0].plot([],[], 'ro') # depot
+		self.ax[0,0].set_title("Nearest Neighbour Algorithm")
+		self.ax[0,1].set_title("Minimum Spanning Tree")
+		self.ax[1,0].set_title("Simulated Annealing")
 		self.ax[1,1].set_title("Optmimization")
 
-	def plot(self, cities, path: list):
-		x = []
-		y = []
-
+	def get_xy(self,cities,p_path:list):
+		x, y = [], []
+		path = p_path[:]
 		path.append(path[0])
 		for index in range(0, len(path)):
 			city = cities[path[index]]
 			x.append(city.x)
 			y.append(city.y)
-		
+		return x,y
+
+	def set_lim(self,x0,x1,y0,y1,ax):
+		ax.set_xlim(x0, x1)
+		ax.set_ylim(y0, y1)
+
+	def plot(self, cities, path: list):
+		x,y = self.get_xy(cities,path)
 		self.cyan_line.set_data(x, y)
 		self.red_line.set_data([cities[0].x],[cities[0].y])
-
-		self.ax[0,0].set_xlim(0, (max(x)+2) * 1.1)
-		self.ax[0,0].set_ylim(0, (max(y)+2)* 1.1)
 		self._flush()
 	
 	def plot_final(self,cities,path:list):
-		x = []
-		y = []
-
-		path.append(path[0])
-		for index in range(0, len(path)):
-			city = cities[path[index]]
-			x.append(city.x)
-			y.append(city.y)
-		
+		x,y = self.get_xy(cities,path)
 		self.cyan_line.set_data([], [])
 		self.green_line.set_data(x,y)
 		self.red_line.set_data([cities[0].x],[cities[0].y])
-
-		self.ax[0,0].set_xlim(0, (max(x)+2) * 1.1)
-		self.ax[0,0].set_ylim(0, (max(y)+2)* 1.1)
 		self._flush()
 
-	def plot_mst(self,graph):
+	def plot_mst(self,graph,x,y,cities):
 		lines = graph.minimumSpanningTree()[1]
-		c = [(1,0,0,1) for i in range(len(lines))]
-		lc = mc.LineCollection(lines, colors=c, linewidths=3)
-		self.ax[0,0].add_collection(lc)
+		c = [(0,0,1,1) for i in range(len(lines))]
+		lc = mc.LineCollection(lines, colors=c, linewidths=1)
+		self.ax[0,1].add_collection(lc)
+		self.ax[0,1].plot(x,y, 'bo')
+		self.ax[0,1].plot([cities[0].x],[cities[0].y],'ro')
+
+	def plot_nna(self,x,y,cities):
+		self.ax[0,0].plot(x,y, 'yo-')
+		self.ax[0,0].plot([cities[0].x],[cities[0].y],'ro')
 
 	def show(self,cities,history,graph):
+		x,y = self.get_xy(cities,history[0])
+		self.set_lim(0, (max(x)+2) * 1.1, 0, (max(y)+2)* 1.1,self.ax[0,0])
+		self.set_lim(0, (max(x)+2) * 1.1, 0, (max(y)+2)* 1.1,self.ax[0,1])
+		self.set_lim(0, (max(x)+2) * 1.1, 0, (max(y)+2)* 1.1,self.ax[1,0])
+
+		self.plot_nna(x,y,cities)
+		self.plot_mst(graph,x,y,cities)
 		for path in history[:-1]:
 			self.plot(cities,path)
 			time.sleep(1)
 		self.plot_final(cities,history[-1])
-		self.plot_mst(graph)
 		self.plot_learning(history,graph)
 		self.end()
 
@@ -72,8 +78,8 @@ class DynamicPlot():
 		line_min = self.ax[1,1].axhline(y=graph.path_cost(history[-1]), color='g', linestyle='--')
 		line_mst = self.ax[1,1].axhline(y=graph.minimumSpanningTree()[0], color='b', linestyle='--')
 		line_mst2 = self.ax[1,1].axhline(y=graph.minimumSpanningTree()[0]*2, color='r', linestyle='--')
-		self.ax[1,1].legend([line_init, line_min, line_mst,line_mst2], 
-					['NNA cost', 'SA cost','mst cost lower','2 * mst cost'])
+		self.ax[1,1].legend([line_mst2,line_init, line_min, line_mst], 
+					['2 * mst','NNA cost', 'SA cost','mst cost'])
 		self.ax[1,1].set(xlabel='iteration', ylabel='cost')
 
 	def _flush(self):
