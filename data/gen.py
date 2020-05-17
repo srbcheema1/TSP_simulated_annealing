d290a694d52a6810c31b4da27c388e39d4d1bcf9
Sarbjit <srbcheema1@gmail.com>
2020-05-17 16:26:05 +0530

    data changes

--- data/gen.py
+++ data/gen.py
@@ -1,7 +1,11 @@
 from random import randint
+import sys
 
 n = 30
+if(len(sys.argv) == 2):
+	n = int(sys.argv[1])
+
 for _ in range(n):
-  a = randint(1,50)
-  b = randint(1,50)
-  print(a,b)
+	a = randint(1,50)
+	b = randint(1,50)
+	print(a,b)
--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from dynamic_plot import DynamicPlot
 
 
 def main():
-    cities = City.load_cities('./data/data1.txt')
-    graph = Graph(cities)
-    init_sol = graph.nearestNeighbourSolution()
-    history = SimulatedAnnealing(graph, init_sol, 0.9998, 10, 0.0000001, 1000000).anneal()
-    DynamicPlot().show(cities,history,graph)
+	cities = City.load_cities('./data/data50.txt')
+	graph = Graph(cities)
+	init_sol = graph.nearestNeighbourSolution()
+	history = SimulatedAnnealing(graph, init_sol, 0.9998, 10, 0.0000001, 1000000).anneal()
+	DynamicPlot().show(cities,history,graph)
 
 
 if __name__ == "__main__":
-    main()
+	main()
